9bd79bb04914c9b636c84c4dd3bf2283c0800892
developerpalash-tech <ceo.spglobal@gmail.com>
2025-08-21 21:24:07 +0600

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -691,6 +691,19 @@ function App() {
 // let person = new Array()
 // console.log(person);
 
+
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+
+Person.prototype.nationality = "English";
+
+const myFather = new Person("John", "Doe", 50, "blue");
+console.log(myFather.nationality)
+
   return (
     <>
       <h1 className='text-red-400'>My name is </h1>
