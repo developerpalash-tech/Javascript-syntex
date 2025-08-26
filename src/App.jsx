b70f45c64f52fb5a22a81ee53f23a1dfb33c371f
developerpalash-tech <ceo.spglobal@gmail.com>
2025-08-26 16:11:06 +0600

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -956,8 +956,27 @@ console.log(result);
 // printName()
 
 
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
+}
 
+let myPromise = new Promise(function(myResolve, myReject) {
+  let x = 0;
 
+// some code (try to change x to 5)
+
+  if (x == 0) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
+  }
+});
+
+myPromise.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
+console.log(myPromise)
   return (
     <>
       <h1 className='text-red-400'>My name is </h1>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <p id="demo"></p>
     <App />
   </StrictMode>,
 )
